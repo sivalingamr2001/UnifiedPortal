e77f4368f4d86095b92f6c18724833ac1f5457a8
Sivalingam R <sivalinga1902@gmail.com>
2026-08-30 08:42:15 +0530

    add rbac

---
+++ RBAC.sql
@@ -0,0 +1,7 @@
+select * from JAN_USER_SESSIONS;
+select * from JAN_ROLE_MENU;
+select * from JAN_ROLES;
+select * from jan_menus;
+select * from jan_modules;
+select * from JAN_USER_MASTER;
+select * from jan_role_modules;
